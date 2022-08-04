7ef46a542d32445573c5d11744ae6733c54d8db2
glitched98 <81384403+glitched98@users.noreply.github.com>
2022-08-04 02:52:40 -0400

    Update queries.sql
    Working Queries

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -17,39 +17,57 @@ INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel,
                         disk_available)
 VALUES ('2019-05-29 15:01:00.000', 1, 200000, 90, 4, 2, 3);
 
-CREATE TABLE temp AS
+SELECT date_trunc('hour', timestamp) + date_part('minute', timestamp):: int / 5 * interval '5 min' 
+FROM host_usage;
+CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
+$$
+BEGIN
+    RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+END;
+$$
+    LANGUAGE PLPGSQL;
+    
 SELECT
-	hostname,
-	id,
-	total_mem,
-	timestamp as timestamp,
-	CAST(memory_free*1024 AS INT) AS memory_free
+    FIRST_VALUE(cpu_number) OVER (
+    PARTITION BY cpu_number
+    ORDER BY
+    total_mem DESC
+    AS cpu_number,
+    id AS host_id,
+    total_mem
 FROM
-	PUBLIC.host_info;
-
-ALTER TABLE temp
-ADD COLUMN rounded timestamp;
-ALTER TABLE temp
-ADD COLUMN memPercentNum NUMERIC;
-ALTER TABLE temp
-ADD COLUMN memPercentUsed INT;
-
-UPDATE temp
-SET rounded = date_trunc('hour', timestamp) + INTERVAL '5 min' * ROUND(date_part('minute', timestamp) / 5.0);
-UPDATE temp
-SET memPercentNum = CAST(memory_free AS NUMERIC)/total_mem*100;
-UPDATE temp
-SET memPercentUsed=CAST(100 - percent_mem_numeric AS INT);
-
-CREATE VIEW memAvg AS
+    host_info;
+   
 SELECT
-	hostname AS host_name,
-	total_mem AS total_memory,
-	CAST(AVG(percent_mem_used) AS SMALLINT) AS used_memory_percentage
-FROM
-	avg_mem_temp
+    usages.host_id,
+    infos.hostname,
+    rounder(usages.timestamp) AS times,
+    AVG((infos.total_mem - usages.memory_free)/infos.total_mem*100) AS avg_used_mem
+FROM host_info AS infos INNER JOIN host_usage AS usages ON infos.id = usages.host_id
+
 GROUP BY
-	id,hostname,total_mem;
+	host_id,
+	round5(
+	CAST(timestamp AS timestamp)
+  	);
+
+--Question 3--
+SELECT
+	host_id,
+	round5(
+	CAST(timestamp AS timestamp)
+  	) AS timestamp,
+  	COUNT(timestamp) AS num_data_points
+FROM
+  	host_usage
+GROUP BY
+  	host_id,
+  	round5(
+    	CAST(timestamp AS timestamp)
+  	)
+HAVING
+  	COUNT(timestamp) < 3;
+
 
 
 
