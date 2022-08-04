3122609f3945981733af830af4e775b9f0762819
glitched98 <81384403+glitched98@users.noreply.github.com>
2022-08-03 22:56:54 -0400

    Update host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [[ $# -ne 5 ]]; then
+	echo "Invalid Number of Arguements used"
+	exit 1
+fi
+
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+lscpu_out=$(lscpu)
+cpu_number=$(echo "$lscpu_out" | grep "^CPU(s):" | awk '{ print $2 }')
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | grep "^Model name" | awk '{ $1=$2="";  print }' | sed 's/^[[:space:]]*//')
+cpu_mhz=$(echo "$lscpu_out" | grep "^CPU MHz:" | awk '{ print $3 }')
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2" | awk '{print $3}' | xargs)
+total_mem=$(grep "^MemTotal" /proc/meminfo | awk '{ print $2 }')
+timestamp=$(date -u +"%Y-%m-%d %H:%M:%S")
+
+psql -h $psql_host -p $psql_port -U $psql_user -w -d $db_name
+
+insert into host_info (
+	hostname, cpu_count, cpu_arch, cpu_model, cpu_mhz, l2_cache, mem_total, curr_time
+)
+values 
+	(
+		'$hostname', $cpu_count, '$cpu_arch', '$cpu_mdl', $cpu_mhz, $l2_cache, $mem_total, '$curr_time'
+	);
+
+exit 0
